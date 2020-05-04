aea4b248321faf03a11e9cba7296fc6f0a7f3cec
Noah Franco <noah@macbooks-mbp.lan>
2020-05-04 13:42:32 -0400

    cleaned codebase

--- index.js
+++ index.js
@@ -3,9 +3,3 @@ require("dotenv").config()
 const PORT = process.env.PORT || 3333
 
 server.listen(PORT, () => console.log(`API running on port ${PORT}`))
-
-// const server = require("./server.js")
-// require("dotenv").config()
-// const PORT = process.env.PORT || 3000
-
-// server.listen(PORT, () => console.log(`API running on port ${PORT}`))
