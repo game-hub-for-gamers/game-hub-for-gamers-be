63a3fca68983210b788c8c6455005f675ec05781
Noah Franco <noah@macbooks-mbp.lan>
2020-05-28 09:40:16 -0400

    log in is working and getting back token

--- Routes/user.js
+++ Routes/user.js
@@ -4,6 +4,7 @@ const getToken = require("../middleware/token");
 
 const UserDB = require("../models/userModels");
 
+// get users endpoint
 router.get("/get", async (req, res) => {
   // http://localhost:3333/api/user/get
   try {
@@ -14,43 +15,7 @@ router.get("/get", async (req, res) => {
   }
 });
 
-// register enddpoint
-// router.post("/register", async (req, res) => {
-//   // http://localhost:3333/api/user/register
-//   const newUser = req.body;
-
-//   if (!newUser) {
-//     res.status(404).json({ err: "Please provide the name" });
-//   } else {
-//     try {
-//       const user = await UserDB.add(newUser); // await for the promise
-//       res.status(201).json(user);
-//     } catch (err) {
-//       res.status(500).json({ err: "Error in adding user" });
-//     }
-//   }
-// });
-
-// login endpoint
-// router.post("/login", async (req, res) => {
-//   // http://localhost:3333/api/user/login
-//   // async creates a promise (true/false)
-
-//   const { username } = req.body;
-//   let { password } = req.body;
-
-//   if (!username) {
-//     res.status(404).json({ err: "Please provide the name" });
-//   } else {
-//     try {
-//       const newUser = await UserDB.findbyId({ username }); // await for the promise
-//       res.status(201).json(newUser);
-//     } catch (err) {
-//       res.status(500).json({ err: "Error in adding user" });
-//     }
-//   }
-// });
-
+// register endpoint
 router.post("/register", async (req, res) => {
   // http://localhost:3333/api/user/register
   let newUser = req.body;
@@ -70,15 +35,16 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// login endpoint
 router.post("/login", (req, res) => {
-  // localhost:9000/api/auth/login
-  // implement login
+  // http://localhost:3333/api/user/login
+
   let { username, password } = req.body;
 
   if (!username && !password) {
     res.status(401).json({ error: "Wrong password or username" });
   } else {
-    UserDB.findbyId({ username, password })
+    UserDB.findbyId({ username })
       .first() // passing in the first value in our object
       .then((user) => {
         if (user && bcrypt.compareSync(password, user.password)) {
@@ -98,18 +64,5 @@ router.post("/login", (req, res) => {
       });
   }
 });
-
-// *** HASH ***
-
-// router.get("/hash", (req, res) => {
-//   const password = req.headers.authorization;
-
-//   if (password) {
-//     const hash = bcrypt.hashSync(password, 10); // for the number of rounds to use when generating a salt (10)
-//     res.status(200).json({ hash });
-//   } else {
-//     res.status(400).json({ error: "Please provide credentials" });
-//   }
-// });
 
 module.exports = router;
