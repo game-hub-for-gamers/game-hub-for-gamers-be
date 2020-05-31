8945ceb18dc7f8a6110a5ed496de7a83a393d36f
efrain <efarinva98@gmail.com>
2020-05-31 01:56:52 -0400

    heroku setting up

--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,15 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    client: "pg",
+    client: "postgres",
     connection: {
-      host: process.env.POSTGRES_DEV_HOST,
-      port: process.env.POSTGRES_DEV_PORT,
-      user: process.env.POSTGRES_DEV_USER,
-      password: process.env.POSTGRES_DEV_PASSWORD,
-      database: process.env.POSTGRES_DEV_DATABASE,
+      host:"localhost",
+      port:"5432",
+      user: "postgres",
+      password: "pass",
+      database: "game",
     },
+    // process.env.POSTGRES_DEV_DATABASE,
     migrations: {
       directory: "./data/migrations",
     },
